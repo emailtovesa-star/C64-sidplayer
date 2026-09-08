e86ada36cb079269593ac3146a9d0ac16596ef2b
emailtovesa-star <emailtovesa@gmail.com>
2026-09-08 21:13:25 +0300

    Add files via upload

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,4 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-rootProject.name = "C64SIDPlayerSimple"
+pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
+rootProject.name = "C64SIDPlayerV33"
 include(":app")
