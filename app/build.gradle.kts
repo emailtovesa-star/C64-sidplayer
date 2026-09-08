2ffa287328a2c1e2ae0750a687891d8615e0c607
emailtovesa-star <emailtovesa@gmail.com>
2026-09-09 02:21:44 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.c64sidplayersimple"
         minSdk = 24
         targetSdk = 35
-        versionCode = 391
-        versionName = "3.9.1"
+        versionCode = 392
+        versionName = "3.9.2"
     }
 }
 
