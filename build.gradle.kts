2a9fd82f819576d772a0b18c0a1922afee9dd1b5
emailtovesa-star <emailtovesa@gmail.com>
2026-09-08 18:09:48 +0300

    Configure Android application settings in build.gradle.kts
    Updated Android plugin and configuration settings.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,16 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.c64sidplayersimple"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.c64sidplayersimple"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 31
+        versionName = "3.1"
+    }
 }
