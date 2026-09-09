831c9510a57313333d0ceb0bcbf9df6f2b979320
emailtovesa-star <emailtovesa@gmail.com>
2026-09-09 14:54:46 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,18 @@ android {
     namespace = "com.example.c64sidplayersimple"
     compileSdk = 35
     defaultConfig {
+        ndk { abiFilters += listOf("arm64-v8a") }
         applicationId = "com.example.c64sidplayersimple"
         minSdk = 23
         targetSdk = 35
-        versionCode = 399
-        versionName = "3.9.9"
+        versionCode = 400
+        versionName = "4.0"
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
     }
 }
 dependencies { implementation("androidx.webkit:webkit:1.12.1") }
