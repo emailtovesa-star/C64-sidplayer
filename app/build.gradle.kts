4b1b6f41770b6ded6f40ffa8f1b4b13d343b44f6
emailtovesa-star <emailtovesa@gmail.com>
2026-09-09 10:45:30 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "com.example.c64sidplayersimple"
         minSdk = 23
         targetSdk = 35
-        versionCode = 397
-        versionName = "3.9.7"
+        versionCode = 398
+        versionName = "3.9.8"
     }
 }
 dependencies { implementation("androidx.webkit:webkit:1.12.1") }
