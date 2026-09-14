6bc6ce38a29532c3d56c82b76a28b4d2fa574e11
emailtovesa-star <emailtovesa@gmail.com>
2026-09-15 01:12:58 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,9 +7,14 @@ android {
         applicationId = "com.example.c64sidplayersimple"
         minSdk = 23
         targetSdk = 35
-        versionCode = 456
+        versionCode = 453
         versionName = "4.0.23"
     }
-    externalNativeBuild { cmake { path = file("src/main/cpp/CMakeLists.txt"); version = "3.22.1" } }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
 }
 dependencies { implementation("androidx.webkit:webkit:1.12.1") }
