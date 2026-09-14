34ed5715136303e8a4d5dfc589f1a77e94661633
emailtovesa-star <emailtovesa@gmail.com>
2026-09-14 14:02:08 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
         applicationId = "com.example.c64sidplayersimple"
         minSdk = 23
         targetSdk = 35
-        versionCode = 451
+        versionCode = 452
         versionName = "4.0.23"
     }
     externalNativeBuild {
